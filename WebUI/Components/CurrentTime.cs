6f4ade727e7c4030565c099569e658fafdb82521
AntonK <Erusalim12@gmail.com>
2021-02-16 17:01:17 +0300

    Добавьте файлы проекта.

---
+++ WebUI/Components/CurrentTime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Components
+{
+    public class CurrentTime:ViewComponent
+    {
+        public string Invoke()
+        {
+            return $"Текущее время: {DateTime.Now:hh:mm:ss}";
+        }
+    }
+}
---
+++ WebUI/Components/RecordList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Components
+{
+    public class RecordList : ViewComponent
+    {
+        //private readonly AppDbContext _context;
+        //public RecordList(DbContext context)
+        //{
+        //    _context = context;
+        //}
+
+        public IViewComponentResult Invoke()
+        {
+            // todo: через DI получать значение из базы данных и выводить его на View
+            var dt = new List<DateTime> {DateTime.MinValue, DateTime.Now, DateTime.MaxValue};
+            return View("RecordList", dt);
+        }
+    }
+}
