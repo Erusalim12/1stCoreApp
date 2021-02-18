c555baf2f2c716b02cfc3ac61f68b4f2621409ac
AntonK <Erusalim12@gmail.com>
2021-02-18 10:45:38 +0300

    Тесты чтения/записи данных через сервис

--- WebUI.Tests/UnitTest1.cs
+++ WebUI.Tests/UnitTest1.cs
@@ -1,20 +1,48 @@
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WebUI.Controllers;
+using WebUI.Models;
+using WebUI.Services;
 
 namespace WebUI.Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void test1()
+        private DateService service;
+        [TestInitialize]
+        public void Prepare()
         {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            optionsBuilder.UseInMemoryDatabase("testdb");
+            var _dbContext = new ApplicationContext(optionsBuilder.Options);
+            service = new DateService(_dbContext);
         }
 
         [TestMethod]
-        public void AddItemToRepo()
+        public void CanFillData()
         {
+            try
+            {
+                service.Add(DateTime.MinValue);
+                service.Add(DateTime.Now);
+                service.Add(DateTime.MaxValue);
+                service.Add(DateTime.Now);
+            }
+            catch(DbUpdateException)
+            {
+                Assert.Fail("Данные не добавлены");
+            }
 
+            
         }
+        [TestMethod]
+        public void ReadData()
+        {
+            var count = service.GetItemCollection().Count(c => c <= DateTime.Now);
+            Assert.AreEqual(count,3);
+        }
+         
     }
 }
